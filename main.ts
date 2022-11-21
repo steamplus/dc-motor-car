d4d5a854b2ff9d9ba8f44516ab51920a8192ef20
steamplus <69250688+steamplus@users.noreply.github.com>
2022-11-21 12:13:26 +0900

    Here is a small project on Micro:bit with DC motor driver.

--- main.ts
+++ main.ts
@@ -1,3 +1,44 @@
-basic.forever(function () {
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 2) {
+        basic.pause(200)
+        MotorDriver.MotorRun(Motor.A, Dir.forward, 7)
+        MotorDriver.MotorRun(Motor.B, Dir.forward, 7)
+    }
+    if (receivedNumber == 3) {
+        basic.pause(200)
+        MotorDriver.MotorRun(Motor.A, Dir.backward, 5)
+        MotorDriver.MotorRun(Motor.B, Dir.backward, 5)
+    }
+    if (receivedNumber == 4) {
+        basic.pause(200)
+        MotorDriver.MotorStop(Motor.A)
+        MotorDriver.MotorStop(Motor.B)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(2)
+})
+input.onGesture(Gesture.TiltLeft, function () {
 	
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(4)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(3)
+})
+input.onGesture(Gesture.TiltRight, function () {
+	
+})
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    . . # . .
+    # . . . #
+    . # # # .
+    `)
+basic.showIcon(IconNames.Heart)
+basic.showString("Hello!")
+basic.forever(function () {
+    radio.setGroup(1)
 })
